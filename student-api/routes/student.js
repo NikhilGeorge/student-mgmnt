09a8aaaefadd61007a42684b3b61952a683034a5
nikhilgnet <nikhil.gnet@gmail.com>
2019-11-30 12:38:25 +0530

    put added for studnet

--- student-api/routes/student.js
+++ student-api/routes/student.js
@@ -25,15 +25,32 @@ router.get('/viewStudent/**', (req, res) => {
 	
 	//Lookup info in DB
 
-  });
+});
 
 
-  router.post("/addStudent", (req, res, next) => {
+router.post("/addStudent", (req, res, next) => {
  
 	//read product information from request
 	console.log(req.body)
 	
 	connection.query(`INSERT INTO student (username, firstname, lastname, email, password, phone, attendence) VALUES ('${req.body.username}',' ${req.body.firstname}','${req.body.lastname}',' ${req.body.email}','${req.body.password}', ${req.body.phone},${req.body.attendence})`, function (error, results, fields) {
+		if(error){
+			res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+			//If there is error, we send the error in the error section with 500 status
+		} else {
+			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+			//If there is no error, all is good and response is 200OK.
+		}
+	});
+ 
+});
+
+router.put("/updateStudent", (req, res, next) => {
+ 
+	//read product information from request
+	console.log(req.body)
+	
+	connection.query(`UPDATE student set password ='${req.body.password}' where studentid = ${req.body.studentid}`, function (error, results, fields) {
 		if(error){
 			res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
 			//If there is error, we send the error in the error section with 500 status
